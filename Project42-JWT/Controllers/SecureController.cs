a82fcdef653e3820fe060807889a63ac9cd8e700
EdaOzge <eozge.ugurlu@tedu.edu.tr>
2025-01-08 01:00:31 +0300

    Proje dosyası ekle.

---
+++ Project42-JWT/Controllers/SecureController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project42_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is a secure endpoint.");
+        }
+    }
+}
